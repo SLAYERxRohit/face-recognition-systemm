7177a00fcb3beefa3f0737d8146b01e6b0a42179
SLAYERxRohit <rohittinu053@gmail.com>
2026-07-02 22:27:13 +0530

    Fix Railway deployment crashes

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ app.add_url_rule(rule='/app/gender/',
                  methods=['GET','POST'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
